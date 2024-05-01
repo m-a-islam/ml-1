0d73e041d0b039ad5e7c5626f68dae98e0befb3e
muhammad <muhammad.cse36@gmail.com>
2024-05-01 15:39:18 +0200

    fastapi with docker implementation

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+from pydantic import BaseModel
+from starlette.responses import FileResponse
+
+#create fastapip
+app = FastAPI()
+#model = pickle.load(open("decision_tree_1.pkl", "rb"))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class request_model(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+model = GaussianNB()
+model.fit(X, y)
+
+
+@app.post("/predict")
+def predict(data: request_model):
+    test_data = [[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]]
+    prediction = model.predict(test_data)[0]
+    return {"class": iris.target_names[prediction]}
+
+
+@app.get("/")
+def read_root():
+    return FileResponse("templates/index.html")
